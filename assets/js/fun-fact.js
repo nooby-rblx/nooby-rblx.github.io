6ff6fb6b8f225de0673b36411a3693f83c44a1ce
nooby-rblx <109273282+nooby-rblx@users.noreply.github.com>
2022-07-17 05:30:01 +0900

    g

--- assets/js/fun-fact.js
+++ assets/js/fun-fact.js
@@ -1,6 +1,9 @@
 const funfacts = [
-    "he learned html and css for 1 years",
-    ""
+    "He learned the programming languages HTML and CSS in one year.",
+    "He learned the programming language Lua in three years. (Roblox only)",
+    "He paints Pibby's black something-infected character in the Adult Swim video (mostly Roblox).",
+    "He started Roblox in 2017. (True story)",
+    "He has multiple accounts.",
 ];
 
 const random = Math.floor(Math.random() * funfacts.length);
